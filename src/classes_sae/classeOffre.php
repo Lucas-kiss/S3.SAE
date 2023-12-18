6ef602eac9f9629f9fddf9532e92d8c5896e4ec3
Lucas KISS <lkiss@iutbayonne.univ-pau.fr>
2023-12-18 16:59:03 +0100

    Finir la repartion de offre et Debut de la creation des objet

--- src/classes_sae/classeOffre.php
+++ src/classes_sae/classeOffre.php
@@ -1,31 +1,24 @@
 <?php
-/**
- * @file Offre.php
- * @author Fanny CONSTANS
- * @brief Création de la classe Offre
- * @version 1
- * @date 2023-12-18
- * 
- */
 
 class Offre
 {
     /* ATTRIBUTS */
     private $num;
     private $intitule;
-
+    private $desJours;
+    private $desEtudiants;
+    private $mesCriteres;
     /* CONSTRUCTEUR */
-    public function Offre($num, $intitule)
+    public function Offre1($num, $intitule)
     {
         $this->num = $num;
         $this->intitule = $intitule;
     }
-    public function Offre_copie(Offre $uneOffre)
+    public function Offre1_copie(Offre1 $uneOffre1)
     {
-        $this->num = $uneOffre->num;
-        $this->intitule = $uneOffre->intitule;
+        $this->num = $uneOffre1->num;
+        $this->intitule = $uneOffre1->intitule;
     }
-
     /* METHODES */
     // set&get num
     public function set_num(int $num)
@@ -35,8 +28,8 @@ class Offre
     public function get_num()
     {
         return $this->num;
-    }
 
+    }
     // set&get intitule
     public function set_intitule(string $intitule)
     {
@@ -46,57 +39,54 @@ class Offre
     {
         return $this->intitule;
     }
-
     // Associer des jours à l'offre
     public function set_desJours(&$desJours)
     {
         $this->desJours = &$desJours;
     }
-
     public function get_desJours()
     {
         return $this->desJours;
     }
-
     // Associer des étudiants à l'offre
     public function set_desEtudiants(&$desEtudiants)
     {
         $this->desEtudiants = &$desEtudiants;
     }
-
     public function get_desEtudiants()
     {
         return $this->desEtudiants;
     }
-
+    //Associer des criteres a l'offre
     public function set_mesCriteres(&$desCriteres)
     {
         $this->mesCriteres = $desCriteres;
     }
-
     public function get_mesCriteres()
     {
         return $this->mesCriteres;
     }
-    
-    public function delierCriteres(Critere &$desCriteres)
-    {
-        if ($this->mesCriteres != null) {
-            unset($this->mesCriteres);
-            unset($desCriteres->get_monOffre());
-        }
 
-    }
+    // public function delierCriteres(Critere &$desCriteres)
+    // {
+    //     if ($this->mesCriteres != null) {
+    //         unset($this->mesCriteres);
+    //         unset($desCriteres->get_monOffre());
+    //     }
 
-    public function lierCriteres(Critere &$desCriteres)
-    {
-        if ($this->get_mesCriteres() == null && $desCriteres->get_monOffre() == null) {
-            $this->set_mesCriteres($desCriteres);
-            $desCriteres->get_monOffre() = &$this;
-        } else {
-            Offre::delierCriteres($desCriteres);
-        }
-    }
+    // }
+
+    // public function lierCriteres(Critere &$desCriteres)
+    // {
+    //     if ($this->get_mesCriteres() == null && $desCriteres->get_monOffre() == null) {
+    //         $this->set_mesCriteres($desCriteres);
+    //         $desCriteres->get_monOffre() = &$this;
+    //     } else {
+    //         Offre::delierCriteres($desCriteres);
+    //     }
+    // }
+
+
 
 }
 
