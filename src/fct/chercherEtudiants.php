2228d0606a629531e70c50d271f57f205bee21a9
Raphaël Audouard <raudouard@iutbayonne.univ-pau.fr>
2023-12-18 16:21:46 +0100

    Modification de la procédure chercherEtudiants

--- src/fct/chercherEtudiants.php
+++ src/fct/chercherEtudiants.php
@@ -1,14 +1,30 @@
 <?php
-function chercherEtudiants($uneOffre,$combsUnJour,$uneCombDUnJour,$heureDeb,$heureFin,$itJourOffre,$cptEtudDispo) 
+
+// Chercher les étudiants qui correspondent à heureDeb
+function chercherEtudiants($uneOffre, $combsUnJour,
+                           $uneCombDUnJour, $heureDeb,
+                           $heureFin, $itJourOffre,
+                           $cptEtudDispo) 
 {
     $trouveEtu = false;
     foreach ($etu in $combsUnJour.getLstEtudiant()) 
     {
-        $itJourEtu = &$jourATraiter;
+        // Faire pointer un itérateur sur le jour à traiter
+        $itJourEtu = &$jourATraiter; 
         while ($itJourEtu.getJour() != $jourATraiter.getJour())
         {
             $itJourEtu++;
-        } 
+        }
+        // vérifier si les horaires de etu correspondent aux horaires de l'offre
+    }
+    // Si aucun étudiant ne peut travailler à heureDeb
+    if (!$trouveEtu) 
+    {
+        // Ajouter EtuNull dans uneCombDUnJour.lstEtudiant
+        ($uneCombDUnJour.lstEtudiant())[] = $EtuNull;
+        combJour($uneOffre, $combsUnJour,
+                 $uneCombDUnJour, $heureDeb+1,
+                 $heureFin, $itJourOffre);
     }
 }
 
