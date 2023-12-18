fed0ed7dcf9efa3cca00d1c4c77441aa92340557
Raphaël Audouard <raudouard@iutbayonne.univ-pau.fr>
2023-12-18 15:19:39 +0100

    Ajout de la procédure chercherEtudiants

---
+++ src/fct/chercherEtudiants.php
@@ -0,0 +1,12 @@
+<?php
+function chercherEtudiants($uneOffre,$combsUnJour,$uneCombDUnJour,$heureDeb,$heureFin,$itJourOffre,$cptEtudDispo) 
+{
+    $trouveEtu = false;
+    foreach ($etu in $combsUnJour.getLstEtudiant()) 
+    {
+        $jourATraiter
+    }
+}
+
+
+?>
