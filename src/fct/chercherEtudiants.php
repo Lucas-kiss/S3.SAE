4b330196b9840294ee36a3abe3417ef8b1c09a4d
Raphaël Audouard <raudouard@iutbayonne.univ-pau.fr>
2023-12-19 15:47:54 +0100

    Modification procédure chercherEtudiants

--- src/fct/chercherEtudiants.php
+++ src/fct/chercherEtudiants.php
@@ -1,6 +1,32 @@
 <?php
 
-// Chercher les étudiants qui correspondent à heureDeb
+/**
+ * @file    horairesEtuCorrespHorairesOffre.php
+ * 
+ * @author  AUDOUARD Raphaël / 1P'titJob
+ * 
+ * @brief   Définition de la procédure horairesEtuCorrespHorairesOffre.
+ *          Cette procédure permet deChercher les étudiants qui correspondent 
+ *          à heureDeb
+ * 
+ * @version 1
+ * 
+ * @date    19/12/2023
+ * 
+ * @details 
+ * 
+ * @param   $itJourEtu itérateur pointant sur un objet de classe Jour
+ * @param   $trouveEtu de type Booléen
+ * @param   $uneCombDUnJour objet de classe CombJour
+ * @param   $uneOffre objet de classe Offre
+ * @param   $combsUnJour Array objet de classe CombJour
+ * @param   $heureDeb de type Entier
+ * @param   $heureFin de type Entier
+ * @param   $itJourOffre itérateur pointant sur un objet de classe Jour
+ * @param   $cptEtudDispo de type Entier
+ * @param   $etu objet de classe Etudiant
+ */
+
 function chercherEtudiants($uneOffre, $combsUnJour,
                            $uneCombDUnJour, $heureDeb,
                            $heureFin, $itJourOffre,
