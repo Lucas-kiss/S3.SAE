7dbe0fd9ed42e546f737ceaad39b51c6b0096481
Raphaël Audouard <raudouard@iutbayonne.univ-pau.fr>
2023-12-19 15:11:37 +0100

    Procédure vérifier si les horaires d'études correspondent aux horaires d'offre

---
+++ src/fct/horairesEtuCorrespHorairesOffre.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * @file    horairesEtuCorrespHorairesOffre.php
+ * 
+ * @author  AUDOUARD Raphaël / 1P'titJob
+ * 
+ * @brief   Définition de la procédure horairesEtuCorrespHorairesOffre.
+ *          Cette procédure permet de vérifier si les horaires des étudiants 
+ *          correspondent aux horaires de l'offre
+ * 
+ * @version 1
+ * 
+ * @date    19/12/2023
+ * 
+ * @details 
+ * 
+ * @param   $itJourEtu itérateur pointant sur un objet de classe Jour
+ * @param   $trouveEtu de type Booléen
+ * @param   $uneCombDUnJour objet de classe CombJour
+ * @param   $uneOffre objet de classe Offre
+ * @param   $combsUnJour Array objet de classe CombJour
+ * @param   $heureDeb de type Entier
+ * @param   $heureFin de type Entier
+ * @param   $itJourOffre itérateur pointant sur un objet de classe Jour
+ * @param   $cptEtudDispo de type Entier
+ * @param   $etu objet de classe Etudiant
+ */
+
+function horairesEtuCorrespHorairesOffre($itJourEtu, $trouveEtu,
+                                         $uneCombDUnJour, $uneOffre,
+                                         $combsUnJour, $heureDeb, 
+                                         $heureFin, $itJourOffre,
+                                         $cptEtudDispo, $etu)
+{
+    $itCreneauEtu = $itJourEtu.creneau.first();
+    $horaireEtuCorrespond = false;
+    while (($horaireEtuCorrespond == false) or ($itCreneauEtu != $itJourEtu.getCreneau().end())) 
+    {
+        if (($heureDeb >= $itCreneauEtu.getHeureDeb()) && ($heureDeb < $itCreneauEtu.getHeureFin()))
+        {
+            $trouveEtu = true;
+            if ($cptEtudDispo == 1) 
+            {
+                ($itCreneauEtu.lstEtudiant())[] = $etu;
+                combJour($uneOffre,
+                         $uneCombDUnJour,
+                         $heureDeb+1,$heureFin,
+                         $itJourOffre,
+                         $combsUnJour);
+            }
+            else 
+            {
+                $copieUneCombDUnJour = $uneCombDUnJour;
+                combJour($uneOffre,
+                         $copieUneCombDUnJour,
+                         $heureDeb+1,$heureFin,
+                         $itJourOffre,
+                         $combsUnJour);
+            }
+        }
+    }
+}
+
+?>
---
+++ src/fct/html/search/files_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['horairesetucorresphorairesoffre_2ephp_0',['horairesEtuCorrespHorairesOffre.php',['../horaires_etu_corresp_horaires_offre_8php.html',1,'']]]
+];
