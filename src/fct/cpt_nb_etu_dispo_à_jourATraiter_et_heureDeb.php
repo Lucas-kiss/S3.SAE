1e7e2b8d6412d01b4dbfbd1f0b8f92fdad48fa31
Fanny Constans <fconstans@iutbayonne.univ-pau.fr>
2023-12-19 15:03:17 +0100

    création des commentaires des fct calculerCombSemaine et faireComb

--- src/fct/cpt_nb_etu_dispo_à_jourATraiter_et_heureDeb.php
+++ src/fct/cpt_nb_etu_dispo_à_jourATraiter_et_heureDeb.php
@@ -2,14 +2,14 @@
 /**
  * @file    cpt_nb_etu_dispo_à_jourATraiter_et_heureDeb.php
  * @author  Kévin BÉGUINEL
- * @brief   Fonction appelé dans la fonction calculerCombJour()
+ * @brief   Fonction appelée dans la fonction calculerCombJour()
  * Calcule et retourne le nombre d'étudiants dispo durant le jourATraiter (ex: lundi, mardi, etc) à l'heure nommé heureDeb (ex:14)
  * @version 1.0
  * @date    19/12/2023
  * 
  * 
  * @param $uneOffre objet de classe Offre 
- * @param $jourATraiter objet de type Jour correspondant à un jour de la semaine où on chercher à calculer toute les combinaisons du jour
+ * @param $jourATraiter objet de type Jour correspondant à un jour de la semaine où on chercher à calculer toutes les combinaisons du jour
  * @param $heureDeb entier correspondant à une heure de la journée
  * @return int 
  */
--- src/fct/faireComb.php
+++ src/fct/faireComb.php
@@ -1,12 +1,24 @@
 <?php
     include '../classes_sae/CombOffre.php';
 
+/**
+ * @file    faireComb.php
+ * @author  fconstans
+ * @brief   Fonction appelée dans la fonction faireComb()
+ * Fait les combinaisons d'Etudiant pour l'Offre
+ * @version 1.0
+ * @date    19/12/2023
+ * 
+ * 
+ * @param $uneOffre objet de classe Offre 
+ * @param $etuNull un étudiant vide permettant de montrer quand aucun étudiant n'est dispo pour l'heure
+ * 
+ */
     function faireComb($uneOffre, $etuNull) {
         $combsOffre = new CombOffre();
         $combsChaqueJour = array(array()); // liste  de liste combsUnJour
 
         // Chercher toutes les combinaisons
-
         foreach ($uneOffre.get_planning() as $itJourOffre) {
             // Chercher toutes les combinaisons d'un jour
         }
@@ -20,7 +32,4 @@
 
     }
     
-    
-
-
 ?>
