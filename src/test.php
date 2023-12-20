b16d0c688806772875c22a1d7b5cce27e3248b31
Fanny Constans <fconstans@iutbayonne.univ-pau.fr>
2023-12-20 11:14:01 +0100

    Modification et creation des liens entre les classes et fonctions

---
+++ src/test.php
@@ -0,0 +1,14 @@
+<?php
+
+    require_once 'objetPhp.php';
+    require_once 'fct/faireComb.php';
+
+
+    // faireComb($lstObjEtudiant[0], $etudiantNull);
+    
+
+
+    //faireComb($lstObjOffre[0], $etuNull)
+
+
+?>
