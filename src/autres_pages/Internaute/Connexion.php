0d685b3b11294b49cf44576cc207cf3aeeebe114
LucaDeBrito <luca.de-brito@etud.univ-pau.fr>
2024-01-26 17:00:35 +0100

    mise en place du css pour les page d'inscription/connexion

--- src/autres_pages/Internaute/Connexion.php
+++ src/autres_pages/Internaute/Connexion.php
@@ -2,30 +2,39 @@
 <html lang="fr">
     <head>
         <meta charset="UTF-8">
-        <title>1PtitJob</title>
+        <title>1PtitJob - Connexion</title>
+        <link rel="stylesheet" href="style.css">
     </head>
     <body>
+        <nav>
+            <div class=wrapper>
+                <img class="logo" src="../../ressources/img/1ptitjob_logo.PNG" width="60" height="60" alt="Logo 1P'titJob"/>
+                <h1 class="titre">1P'titJob</h1>
+                <a href="Connexion.php" class="connexion">Connexion</a>
+            </div>
+        </nav>
+        
         <H1>Connexion</H1>
         <form action="idk.php" method="POST">
-            <table class="noborder">
+            <table>
                 <tbody>
                     <tr>
-                        <th class="noborder"><label for="mail">Adresse e-mail :</label></th>
-                        <td class="noborder"><input type="email" id="mail" name="mail" placeholder="exemple@domaine" required/></td>
+                        <th><label for="mail">Adresse e-mail :</label></th>
+                        <td><input type="email" id="mail" name="mail" placeholder="exemple@domaine" required/></td>
                     </tr>
                     <tr>
-                        <th class="noborder"><label for="MdP">Mot de passe :</label></th>
-                        <td class="noborder"><input type="password" id="MdP" name="MdP" required/></td>
+                        <th><label for="MdP">Mot de passe :</label></th>
+                        <td><input type="password" id="MdP" name="MdP" required/></td>
                     </tr>
                     <tr>
-                        <td class="noborder"></td>
-                        <td class="noborder">
+                        <td></td>
+                        <td>
                             <a>Mot de passe oublié ?</a></br>
                             Pas de compte ? <a href="Inscription.php">S'inscrire</a>
                         </td>
                     </tr>
                     <tr>
-                        <td class="noborder">
+                        <td>
                             <input type="checkbox" id="notrobot" required/>
                             <label for="notrobot">Je ne suis pas un robot</label>
                         </td>
