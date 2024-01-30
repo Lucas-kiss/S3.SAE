5abb47c435fd58ee6b05e1f29538593044e137e9
Raphaël Audouard <raudouard@iutbayonne.univ-pau.fr>
2024-01-30 09:17:53 +0100

    mise en place de la connexion à la bdd, renvoie sur les bons types d'acteurs

--- src/autres_pages/Internaute/Connexion.php
+++ src/autres_pages/Internaute/Connexion.php
@@ -1,52 +1,3 @@
-<<<<<<< HEAD
-<!DOCTYPE html>
-<html lang="fr">
-    <head>
-        <meta charset="UTF-8">
-        <title>1PtitJob - Connexion</title>
-        <link rel="stylesheet" href="style.css">
-    </head>
-    <body>
-        <nav>
-            <div class=wrapper>
-                <img class="logo" src="../../ressources/img/1ptitjob_logo.PNG" width="60" height="60" alt="Logo 1P'titJob"/>
-                <h1 class="titre">1P'titJob</h1>
-                <a href="Connexion.php" class="connexion">Connexion</a>
-            </div>
-        </nav>
-        
-        <H1>Connexion</H1>
-        <form action="idk.php" method="POST">
-            <table>
-                <tbody>
-                    <tr>
-                        <th><label for="mail">Adresse e-mail :</label></th>
-                        <td><input type="email" id="mail" name="mail" placeholder="exemple@domaine" required/></td>
-                    </tr>
-                    <tr>
-                        <th><label for="MdP">Mot de passe :</label></th>
-                        <td><input type="password" id="MdP" name="MdP" required/></td>
-                    </tr>
-                    <tr>
-                        <td></td>
-                        <td>
-                            <a>Mot de passe oublié ?</a></br>
-                            Pas de compte ? <a href="Inscription.php">S'inscrire</a>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <input type="checkbox" id="notrobot" required/>
-                            <label for="notrobot">Je ne suis pas un robot</label>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
-            <input type="submit" value="Se connecter">
-        </form>
-    </body>
-</html>
-=======
 <?
 
 require ("../../ressources/donnees/BDD/bdd.php");
@@ -91,4 +42,3 @@ if (isset($_POST['connexion'])) {
     
 
 ?>
->>>>>>> depotOffreBDD
