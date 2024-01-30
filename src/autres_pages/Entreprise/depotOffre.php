2740e17ec5c1647b2f9b3f2afbc4df35b15b610d
Fanny Constans <fconstans@iutbayonne.univ-pau.fr>
2024-01-30 10:54:02 +0100

    Mise en place du dépôt des offres sur la bdd

--- src/autres_pages/Entreprise/depotOffre.php
+++ src/autres_pages/Entreprise/depotOffre.php
@@ -1,7 +1,7 @@
 <?php
 
     if(isset($_POST["suivant"])) {
-        require ("../../ressources/donnees/BDD/bdd.php");
+        require_once ("../../ressources/donnees/BDD/bdd.php");
         session_start();
 
         ini_set('display_errors', 1);
@@ -20,45 +20,24 @@
             $max = $donnees["MAX(idOffre)"] + 1;
         }
 
-        $dateActuelle = date('yyyy-mm-dd hh:mm:ss');
+        $dateActuelle = date('Y-m-d H:i:s');
         $tauxHoraire = str_replace(",", ".",$_POST["tauxHoraire"]);
         $tauxHoraire = floatval($tauxHoraire);
-
-        $dateTest = "2022-04-07 08:00:00";
-        $dateTestDeb = "2023-01-01";
-        $dateTestFin = "2023-01-10";
-
-        // mettre date en format date
         $intitOffre = $_POST["intitOffre"];
         $dateDeb = $_POST["dateDeb"];
         $dateFin = $_POST["dateFin"];
         $description = $_POST["descrOffre"];
  
-        var_dump($max);
-        var_dump($intitOffre);
-        var_dump($dateTest);
-        var_dump($dateTestDeb);
-        var_dump($dateTestFin);
-        var_dump($nbHeureTotal);
-        var_dump($tauxHoraire);
-        var_dump($description);
-        var_dump($siren);
         
-        // $query = "INSERT INTO Offre VALUES ($max, '" . $_POST["intitOffre"] ."',  $dateActuelle, '". $_POST["dateDeb"] ."', '". $_POST["dateFin"] ."', $nbHeureTotal, $tauxHoraire, '" . $_POST["descrOffre"] ."', 0, 0, $siren);";
-        $query = "INSERT INTO Offre (
-        VALUES ($max, $intitOffre,  $dateTest, $dateTestDeb , $dateTestFin, $nbHeureTotal, $tauxHoraire, $description, 0, 0, $siren)";
-
-
-        $result= mysqli_query($link, $query);
+        $query = "INSERT INTO Offre VALUES ($max,'$intitOffre' , '$dateActuelle', '$dateDeb', '$dateFin', $nbHeureTotal, $tauxHoraire, '$description', 0, 0, $siren)";
+        $res = mysqli_query($link, $query);
         
-
-        // $result= mysqli_query($link, $query);
-
-        if ($result) {
-            echo 'e';
+        if ($res) {
+            echo 'Insertion fonctionnelle';
         }
-
+        else {
+            echo "Insertion n'a pas fonctionné";
+        }
     }
-
         
 ?>
