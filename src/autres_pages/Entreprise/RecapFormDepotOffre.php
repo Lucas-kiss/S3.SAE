86054086ee4d15a42d2421c52f613c0b5dcc45e9
Fanny Constans <fconstans@iutbayonne.univ-pau.fr>
2024-01-30 16:59:30 +0100

    ajout des boutons pour le depot de l'offre + probleme sur la bdd à régler

--- src/autres_pages/Entreprise/RecapFormDepotOffre.php
+++ src/autres_pages/Entreprise/RecapFormDepotOffre.php
@@ -1,3 +1,11 @@
+<?php
+if (isset($_POST["suivant"])) {
+    require_once ("../../ressources/donnees/BDD/bdd.php");
+    session_start();
+
+    if ($_POST["dateDeb"] <= $_POST["dateFin"]) {
+        ?>
+        
 <!DOCTYPE html>
 <html lang="fr">
 
@@ -17,7 +25,7 @@
 
 <body>
 
-    <form action="RecapFormDepotOffre.php" method="POST">
+    <form action="depotOffre.php" method="POST">
 
         <div class="fondForm">
             <H1 class="titreDepot">Dépôt d'offre</H1>
@@ -27,8 +35,7 @@
                     <td><label for="intitOffre">Intitulé de l'offre</label><label class="etoile"> *</label></td>
                 </tr>
                 <tr>
-                    <td><input class="champs" type="text" name="intitOffre" placeholder="Ex : Serveur dans un restaurant"
-                            required />
+                    <td><input disabled class="champsRecap" type="text" name="intitOffre" value= <?php echo $_POST["intitOffre"] ?> required />
                     </td>
                 </tr>
                 <tr>
@@ -37,39 +44,39 @@
                     </td>
                 </tr>
                 <tr>
-                    <td><input class="boiteTexte" type="date" name="dateDeb" min=<?php echo date("Y-m-d") ?> required /></td>
+                    <td><input disabled class="dateRecap" type="date" name="dateDeb" value=<?php echo $_POST["dateDeb"] ?> min=<?php echo date("Y-m-d") ?> required /></td>
                 </tr>
                 <tr>
                     <td><label for="dateFin">Date de fin </label><label class="etoile"> *</label></td>
                 </tr>
                 <tr>
-                    <td><input class="date" type="date" name="dateFin" min=<?php echo date("Y-m-d") ?> required /></td>
+                    <td><input disabled class="dateRecap" type="date" name="dateFin" value=<?php echo $_POST["dateFin"] ?> min=<?php echo date("Y-m-d") ?> required /></td>
                 </tr>
                 <tr>
                     <td><label for="tauxHoraire">Taux horaire (valeur nette en €)</label><label class="etoile">
                             *</label></td>
                 </tr>
                 <tr>
-                    <td><input class="champs" type="text" name="tauxHoraire" pattern="[0-9]{2},[0-9]{2}"
-                            placeholder="Ex : 11,50" title="Format monétaire (00,00)" required />
+                    <td><input disabled class="champsRecap" type="text" name="tauxHoraire" pattern="[0-9]{2},[0-9]{2}"
+                    value= <?php echo $_POST["tauxHoraire"] ?> title="Format monétaire (00,00)" required />
                     </td>
                 </tr>
                 <tr>
                     <td><label for="descrOffre">Description de l'offre</label><label class="etoile"> *</label></td>
                 </tr>
                 <tr>
-                    <td><textarea name="descrOffre" required cols="20" rows="12"
-                            placeholder="Ex : Nous recherchons un serveur les soirs de semaine..."></textarea></td>
+                    <td><textarea disabled class="textAreaRecap" name="descrOffre" required cols="20" rows="12"
+                    placeholder= <?php echo $_POST["descrOffre"] ?> required></textarea></td>
                 </tr>
                 <td>
                     <label class="etoile">* </label><label class="champsObl">Champs obligatoires<label>
                 </td>
                 <tr>
                     <td>
-                        <input type="button" class="btnSuivant" name="annuler" value="Annuler" onclick="history.back()">
+                        <input type="button" class="btnSuivant" name="modifier" value="Modifier" onclick="history.back()">
                     </td>
                     <td>
-                        <input type="submit" class="btnSuivant" name="suivant" value="Suivant">
+                        <input type="submit" class="btnSuivant" name="suivant" value="Valider">
                     </td>
                 </tr>
             </table>
@@ -79,3 +86,13 @@
 </body>
 
 </html>
+
+<?php
+    }
+    else {
+        echo "<div class='alert alert-danger' role='alert'>
+        Erreur : la date de début de l'offre doit être inférieure à celle de fin ! </div>";
+    }
+}
+?>
+
