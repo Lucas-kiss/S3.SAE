20487fd328242b5d7a64e4f4dbc959343668c207
Raphaël Audouard <raudouard@iutbayonne.univ-pau.fr>
2024-01-31 12:11:11 +0100

    Modification de fichiers utilisant la base de données

--- src/autres_pages/Internaute/Connexion.php
+++ src/autres_pages/Internaute/Connexion.php
@@ -1,6 +1,6 @@
 <?php
 
-require ("../../ressources/donnees/BDD/bdd.php"); // connexion à la base de données, bdd.php pour lakartxela, bdd_MAMP.php pour MAMP
+require ("../../ressources/donnees/BDD/bdd_MAMP.php"); // connexion à la base de données, bdd.php pour lakartxela, bdd_MAMP.php pour MAMP
 session_start();
 
 if (isset($_POST['connexion'])) {
@@ -15,28 +15,38 @@ if (isset($_POST['connexion'])) {
     $mail = $_POST['mail'];
     $mdp = hash('sha1', $_POST['MdP']);
 
+    $trouve = false; // Retourne vrai si un compte lié au mail et au mot de passe saisis a été trouvé, faux sinon
+
+    // Table Entreprise
     $queryEntr = 'SELECT * FROM Entreprise WHERE mailResponsable = "'.$mail.'" AND mdpResponsable = "'.$mdp.'";';
     $resultEntr = mysqli_query($link, $queryEntr);    
 
     if ($link) {    // si la requête a fonctionné
-        if ($link->affected_rows> 0) {    // si la requête a retourné au moins un enregistrement
+        if ($link->affected_rows> 0) {    // si la requête a retourné au moins un enregistrement d'entreprise
+            $trouve = true;
             while ($donnees=mysqli_fetch_assoc($resultEntr)) {
                 $_SESSION['siren'] = intval($donnees["siren"]);
                 header ('location: ../Entreprise/FormDepotOffre.php');
             }
         }
-        $queryEtud = 'SELECT * FROM Etudiant WHERE mailEtud = "'.$mail.'" AND mdpEtud = "'.$mdp.'";';
-        $resultEtud = mysqli_query($link, $queryEtud);  
+    }
 
-        if ($link->affected_rows> 0) {    // si la requête a retourné au moins un enregistrement
+    // Table Etudiant
+    $queryEtud = 'SELECT * FROM Etudiant WHERE mailEtud = "'.$mail.'" AND mdpEtud = "'.$mdp.'";';
+    $resultEtud = mysqli_query($link, $queryEtud); 
+
+    if ($link) {    // si la requête a fonctionné
+        if ($link->affected_rows> 0) {    // si la requête a retourné au moins un enregistrement d'étudiant
+            $trouve = true;
             while ($donnees=mysqli_fetch_assoc($resultEtud)) {
                 $_SESSION['ine'] = $donnees["ine"];
                 header ('location: ../Etudiant/FormulaireEtudiant.php');
             }
         } 
     }
-    else {
-        echo "<p>Aucun compte lié au mail et mdp saisis</p>";
+
+    if (!($trouve)) { 
+        echo "<p>Aucun compte lié au mail et au mot de passe saisis</p>";
     }
 }
     
